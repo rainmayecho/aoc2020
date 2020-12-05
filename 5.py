a05ecb75561c1dc25c9fba30741970a040ec76a2
Vishnu <sanguinex9@gmail.com>
2020-12-05 00:05:58 -0600

    cleaner 5 pt 2

--- 5.py
+++ 5.py
@@ -10,7 +10,7 @@ test = [
 ]
 
 def solve_1(data):
-    best, pairs = 0, []
+    best, sids = 0, []
     for ticket in data:
         rd, sd = ticket[:7], ticket[7:]
         l, r = 0, 127
@@ -30,27 +30,24 @@ def solve_1(data):
                 ll = mid
         col = rr
         sid = row * 8 + col
-        pairs.append((row, col))
+        sids.append(sid)
         best = max(best, sid)
-    return best, pairs
+    return best, sids
 
 make_seat_id = lambda p: p[0] * 8 + p[1]
 
-def solve_2(rc_pairs):
-    rc_pairs.sort()
-    first = rc_pairs[0]
-    prev = (first[0], first[1], make_seat_id(first))
-    for p in rc_pairs[1:]:
-        sid = make_seat_id(p)
-        pr, pc, ps = prev
-        if pr == p[0] and sid > ps+1:
-            return sid-1
-        prev = (p[0], p[1], sid)
+def solve_2(sids):
+    sids.sort()
+    prev = sids[0]
+    for sid in sids[1:]:
+        if (sid - prev) > 1:
+            return sid - 1
+        prev = sid
 
 
 if __name__ == "__main__":
     with open(f"{DAY}.in", "r") as f:
         data = list(readfile(f, mapper = lambda x: x[0].rstrip()))
-        best, pairs = solve_1(data)
+        best, sids = solve_1(data)
         print(best)
-        print(solve_2(pairs))
+        print(solve_2(sids))
